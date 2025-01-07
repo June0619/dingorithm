cb2562686ebd1f55bd2bffa0d9651c196b63d5af
June0619 <jjw5050@hotmail.com>
2025-01-07 12:52:49 +0900

    최댓값 찾기

--- 1st_week/01_01_find_max_num.py
+++ 1st_week/01_01_find_max_num.py
@@ -1,1 +1,16 @@
-print("hello")
+def find_max_num(array):
+    max_value = -999
+
+    # for i in range(len(array)):
+    #   if max_value < array[i]:
+    #     max_value = array[i]
+
+    for num in array:
+      if num > max_value:
+        max_value = num
+
+    return max_value
+
+print("정답 = 6 / 현재 풀이 값 = ", find_max_num([3, 5, 6, 1, 2, 4]))
+print("정답 = 6 / 현재 풀이 값 = ", find_max_num([6, 6, 6]))
+print("정답 = 1888 / 현재 풀이 값 = ", find_max_num([6, 9, 2, 7, 1888]))
