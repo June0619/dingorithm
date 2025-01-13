00046bd0a3c108b5c789a9f6ee5a5fe2aac5f0d7
June0619 <jjw5050@hotmail.com>
2025-01-13 16:55:31 +0900

    소수 찾기 1

--- 1st_week/01_06_prime_number_1.py
+++ 1st_week/01_06_prime_number_1.py
@@ -1,16 +1,22 @@
-def find_prime_list_under_number(number):
-  prime_arr = []
-  for i in range(number):
-    if i > 1 and is_prime(i):
-      prime_arr.append(i)
-  return prime_arr
+start_range, end_range = map(int, input().split(' '))
 
-def is_prime(number):
-  for i in range(2, number // 2):
-    if number % i == 0:
-      return False
+prime_arr = [0] * end_range
 
-  return True
+def print_(result):
+  if start_range <= result:
+    print(result)
 
-result = find_prime_list_under_number(20)
-print(result)
+for idx in range(end_range):
+  num = idx + 1
+  if num == 1:
+    continue
+  if num == 2:
+    for j in range(3, end_range, 2):
+      prime_arr[j] += 1
+    print_(num)
+    continue
+  if prime_arr[idx] == 0:
+    for k in range(idx+num, end_range, num):
+      prime_arr[k] += 1
+    print_(num)
+    continue
