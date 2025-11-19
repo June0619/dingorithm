60519fb4f10f41bd7ae2c42bb843db10d33ad7b5
June0619 <jjw5050@hotmail.com>
2025-11-19 13:45:26 +0900

    02 is_palindrome_recursive

---
+++ 2nd_week/02_is_palindrome_recursive.py
@@ -0,0 +1,13 @@
+input_string = "abcdba"
+
+def is_palindrome(string):
+
+    if len(string) <= 1:
+        return True
+
+    if string[0] != string[-1]:
+        return False
+    else:
+        return is_palindrome(string[1:-1])
+
+print(is_palindrome(input_string))
