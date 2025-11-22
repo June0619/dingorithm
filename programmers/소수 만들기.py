64a838b397725f40f8ac7888b389fdd1187002a3
June0619 <jjw5050@hotmail.com>
2025-11-22 22:33:00 +0900

    소수 만들기 개선

--- programmers/소수 만들기.py
+++ programmers/소수 만들기.py
@@ -1,24 +1,25 @@
+import math
+
 def is_prime(num):
-    for i in range(2, num):
+    if num < 2:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
         if num % i == 0:
             return False
-    else:
-        return True
+    return True
 
-def bfs(nums, arr):
-    arr.append(nums[0])
+def dfs(nums, start, depth, current_sum):
+    # 3개를 고른 경우
+    if depth == 3:
+        return 1 if is_prime(current_sum) else 0
 
-    if len(arr) == 3:
-        return 1 if is_prime(sum(arr)) else 0
-    else:
-        remain_length = len(nums) - (3 - len(arr))
-        answer_count = 0
-        for i in range(1, remain_length + 1):
-            answer_count += bfs(nums[i:], arr.copy())
-        return answer_count
+    count = 0
+    # 현재 위치부터 남은 숫자들 순회
+    for i in range(start, len(nums)):
+        count += dfs(nums, i + 1, depth + 1, current_sum + nums[i])
+    return count
 
 def solution(nums):
-    summary = 0
-    for i in range(len(nums) - 2):
-        summary += bfs(nums[i:], [])
-    return summary
+    return dfs(nums, 0, 0, 0)
+
+print(solution([1,2,7,6,4]))
