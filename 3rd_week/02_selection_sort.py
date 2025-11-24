37826b8e6cd27000185ab9c3e8deb19a78c72be8
June0619 <jjw5050@hotmail.com>
2025-11-24 13:27:39 +0900

    03_02_selection_sort

---
+++ 3rd_week/02_selection_sort.py
@@ -0,0 +1,14 @@
+
+def selection_sort(array):
+    for i in range(len(array) - 1):
+        min_idx = i
+        for j in range(i, len(array)):
+            if array[j] < array[min_idx]:
+                min_idx = j
+        array[i], array[min_idx] = array[min_idx], array[i]
+
+    return array
+
+print("정답 = [1, 2, 4, 6, 9] / 현재 풀이 값 = ",selection_sort([4, 6, 2, 9, 1]))
+print("정답 = [-1, 3, 9, 17] / 현재 풀이 값 = ",selection_sort([3,-1,17,9]))
+print("정답 = [-3, 32, 44, 56, 100] / 현재 풀이 값 = ",selection_sort([100,56,-3,32,44]))
