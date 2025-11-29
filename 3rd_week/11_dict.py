5314be74cd70938b8e77fc9d08fb7584516cc50b
June0619 <jjw5050@hotmail.com>
2025-11-29 21:30:57 +0900

    11_dict

---
+++ 3rd_week/11_dict.py
@@ -0,0 +1,43 @@
+class Dict:
+    def __init__(self):
+        self.items = [None] * 8
+
+    def put(self, key, value):
+        idx = hash(key) % len(self.items)
+        self.items[idx] = value
+        return
+
+    def get(self, key):
+        idx = hash(key) % len(self.items)
+        return self.items[idx]
+
+my_dict = Dict()
+my_dict.put("test", 3)
+print(my_dict.get("test"))  # 3이 반환되어야 합니다!
+
+class LinkedTuple:
+    def __init__(self):
+        self.items = []
+
+    def add(self, key, value):
+        self.items.append((key, value))
+
+    def get(self, key):
+        for k, v in self.items:
+            if k == key:
+                return v
+
+class LinkedDict:
+    def __init__(self):
+        self.items = []
+        for i in range(8):
+            self.items.append(LinkedTuple())
+
+    def put(self, key, value):
+        index = hash(key) % len(self.items)
+        self.items[index].add(key, value)
+        return
+
+    def get(self, key):
+        index = hash(key) & len(self.items)
+        return self.items[index].get(key)
