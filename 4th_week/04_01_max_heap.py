d5faa398d242337ebf402de326d4b79c753eb145
June0619 <jjw5050@hotmail.com>
2025-11-30 23:48:44 +0900

    04_01_max_heap

--- 4th_week/04_01_max_heap.py
+++ 4th_week/04_01_max_heap.py
@@ -14,9 +14,47 @@ class MaxHeap:
 
         return
 
+    def delete(self):
+
+        self.items[1], self.items[-1] = self.items[-1], self.items[1]
+
+        deleted = self.items.pop()
+        cur_idx = 1
+        last_idx = len(self.items) - 1
+
+        while cur_idx <= last_idx:
+            l_child_idx = cur_idx * 2
+            r_child_idx = cur_idx * 2 + 1
+
+            max_idx = cur_idx
+
+            if l_child_idx <= last_idx and self.items[cur_idx] < self.items[l_child_idx]:
+                max_idx = l_child_idx
+
+            if r_child_idx <= last_idx and self.items[cur_idx] < self.items[r_child_idx]:
+                max_idx = r_child_idx
+
+            if max_idx == cur_idx:
+                break
+
+            self.items[cur_idx], self.items[max_idx] = self.items[max_idx], self.items[cur_idx]
+            cur_idx = max_idx
+
+        return deleted  # 8 을 반환해야 합니다.
+
 max_heap = MaxHeap()
-max_heap.insert(3)
-max_heap.insert(4)
+max_heap.insert(8)
+max_heap.insert(6)
+max_heap.insert(7)
 max_heap.insert(2)
-max_heap.insert(9)
-print(max_heap.items)  # [None, 9, 4, 2, 3] 가 출력되어야 합니다!
+max_heap.insert(5)
+max_heap.insert(4)
+print(max_heap.items)  # [None, 8, 6, 7, 2, 5, 4]
+print(max_heap.delete())  # 8 을 반환해야 합니다!
+print(max_heap.items)  # [None, 7, 6, 4, 2, 5]
+print(max_heap.delete())  # 8 을 반환해야 합니다!
+print(max_heap.items)
+max_heap.insert(10)
+max_heap.insert(7)
+
+print(max_heap.items)
