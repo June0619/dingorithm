7c7ee96f80b9ca02d467879e3046a11f96ecac28
June0619 <jjw5050@hotmail.com>
2025-12-22 23:47:03 +0900

    wip kakao ex 3

---
+++ 5th_week/09_kakao_ex_03.py
@@ -0,0 +1,37 @@
+
+lines = [
+"2016-09-15 01:00:04.002 2.0s",
+"2016-09-15 01:00:07.000 2s"
+]
+
+def solution(p_lines):
+    for log in p_lines:
+        log = LogInspector(log)
+
+class LogInspector:
+
+    def __init__(self, log_string):
+        log_split = log_string.split(' ')
+        end_time_arr = list(map(int, log_split[1][:8].split(':')))
+        end_time_arr.append(int(log_split[1][9:]))
+
+        end_time = end_time_arr[0] * 60 * 60 * 1000 + end_time_arr[1] * 60 * 1000 + end_time_arr[2] * 1000 + end_time_arr[3]
+
+        duration_str = log_split[2]
+        duration_str = duration_str.replace('s', '')
+
+        duration_arr = list(map(int, duration_str.split('.')))
+        duration = duration_arr[0] * 1000
+        if len(duration_arr) > 1:
+            duration += duration_arr[1]
+
+        start_time = end_time - duration
+
+        start_time_arr = []
+        start_time_arr.append(start_time // (60 * 60 * 1000))
+        start_time -= start_time // (60 * 60 * 1000)
+        start_time_arr.append(start_time // (60 * 1000))
+
+        print(end_time, start_time, duration, start_time_arr)
+
+solution(lines)
